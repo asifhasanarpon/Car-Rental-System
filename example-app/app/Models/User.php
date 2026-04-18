98a04076a0ad941deee51947d684a510a77e9422
Asif Hasan Arpon <143158824+asifhasanarpon@users.noreply.github.com>
2026-04-18 10:21:39 +0600

    Update User.php

--- example-app/app/Models/User.php
+++ example-app/app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -13,32 +13,22 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    // Add this method inside your existing User class
+    public function bookings()
+    {
+    return $this->hasMany(Booking::class);
+    }
     protected function casts(): array
     {
         return [
