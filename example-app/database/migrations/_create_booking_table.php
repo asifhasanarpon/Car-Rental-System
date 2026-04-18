e9a53aeb5db4c9897a82609aa58c2894320334f9
Asif Hasan Arpon <143158824+asifhasanarpon@users.noreply.github.com>
2026-04-18 10:18:18 +0600

    Update _create_booking_table.php

--- example-app/database/migrations/_create_booking_table.php
+++ example-app/database/migrations/_create_booking_table.php
@@ -6,44 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cars', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('brand');
+            $table->string('model_name');
+            $table->string('image_url');
+            $table->decimal('price_per_day', 8, 2);
+            $table->integer('capacity');
+            $table->string('transmission');
+            $table->string('fuel_type');
             $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('cars');
     }
 };
